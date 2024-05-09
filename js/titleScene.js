42cb4655637a92d9d3798734384896f20d03b961
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-05-09 02:26:26 +0000

    u

--- js/titleScene.js
+++ js/titleScene.js
@@ -2,17 +2,23 @@ class TitleScene extends Phaser.Scene {
 
   constructor() {
     super({key: "titleScene"})
+  
+    this.titleSceneAnimation = null
   }
   init(data) {
     this.cameras.main.setBackgroundColor('#ffffff')
   }
   preload() {
     console.log("Title Scene")
+    this.load.video('titleSceneAnimation', './assets/titleSceneAnimation.mp4')
   }
 
 
   create(data) {
-
+    this.titleSceneAnimation = this.add.video(0, 0, 'titleSceneAnimation')
+    this.titleSceneAnimation.x = 1920 / 2
+    this.titleSceneAnimation.y = 1080 / 2
+    this.titleSceneAnimation.play()
   }
   update(time, delta) {
     //pass
