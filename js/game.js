4430bdd228199260c4c266973a6a39eeb54bbb20
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-05-21 02:49:37 +0000

    u

--- js/game.js
+++ js/game.js
@@ -66,4 +66,4 @@ game.scene.add('gameSceneTwo', gameSceneTwo)
 game.scene.add('transitionSceneTwo', transitionSceneTwo)
 game.scene.add('gameSceneThree', gameSceneThree)
 // start scene
-game.scene.start('splashScene')
+game.scene.start('gameSceneThree')
--- js/gameSceneThree.js
+++ js/gameSceneThree.js
@@ -43,7 +43,7 @@ class GameSceneThree extends Phaser.Scene {
     // really just to check if stuff is working
     console.log("Game Scene Three")
     this.load.image('gameSceneThreeBackground', '././assets/gameSceneThreeBackground.webp')
-    this.load.spritesheet('penelope3', '././assets/penelopeSceneTwo.png', {
+    this.load.spritesheet('penelope3', '././assets/penelope3Spritesheet.png', {
       frameWidth: 1000,
       frameHeight: 1000
     })
@@ -58,30 +58,49 @@ class GameSceneThree extends Phaser.Scene {
     this.gameSceneThreebackground = this.add.image(0, 0, 'gameSceneThreeBackground')
     this.gameSceneThreebackground.x = 1920 / 2
     this.gameSceneThreebackground.y = 1080 / 2
-    this.penelope = this.physics.add.sprite(1920 / 2, 1080 - 300, 'penelope3').setScale(0.1)
+    this.penelope = this.physics.add.sprite(1920 / 2, 1080 - 300, 'penelope3').setScale(0.2)
     // gives penelope a slight bounce, you can see when loads in
 
     //  220x104 original size, 110x52 new size, the 'true' argument means "center it on the gameobject"
-    this.penelope.setSize(300, 400, true)
+    this.penelope.setSize(150, 200, true)
     // changes the position of the hitbox for the sprite 
-    this.penelope.body.setOffset(380, 570)
+    this.penelope.body.setOffset(200, 650)
     this.penelope.setCollideWorldBounds(true)
 
     this.seedGroup = this.add.group()
     // since penelope is not collecting anything, the animation would remain the same, this animation is for penelope walking
-    //this.anims.create({
-      //key: "penelope_anim",
-      //frames: this.anims.generateFrameNumbers("penelope3", {start: 1, end: 5}),
-      //frameRate: 20,
-      //repeat: -1
-    //})
-
-    // frame for standing
-    //this.anims.create({
-      //key: "penelope_anim_standing",
-      //frames: this.anims.generateFrameNumbers("penelope3", 0)
-    //})
-    
+    this.anims.create({
+      key: "penelope_walking_right",
+      frames: this.anims.generateFrameNumbers("penelope3", {start: 5, end: 9}),
+      frameRate: 20,
+      repeat: -1
+    })
+    this.anims.create({
+      key: "penelope_walking_left",
+      frames: this.anims.generateFrameNumbers("penelope3", {start: 0, end: 4}),
+      frameRate: 20,
+      repeat: -1
+    })
+    // frame for  standing
+    this.anims.create({
+      key: "penelope_anim_standing_right",
+      frames: this.anims.generateFrameNumbers("penelope3", {start: 19, end: 19})
+    })
+    // frame for  standing
+    this.anims.create({
+      key: "penelope_anim_standing_left",
+      frames: this.anims.generateFrameNumbers("penelope3", {start: 20, end: 20})
+    })
+    this.anims.create({
+      key: "penelope_attack_right",
+      frames: this.anims.generateFrameNumbers("penelope3", {start: 10, end: 18}),
+      frameRate: 20,
+    })
+    this.anims.create({
+      key: "penelope_attack_left",
+      frames: this.anims.generateFrameNumbers("penelope3", {start: 21, end: 28}),
+      frameRate: 20,
+    })
     this.cursors = this.input.keyboard.createCursorKeys()
 
     
@@ -97,8 +116,12 @@ class GameSceneThree extends Phaser.Scene {
     this.createSeed()
     this.createSeed()
 
-    this.attackHitBox = this.add.rectangle(0, 0, 110, 80, 0xffffff, 0.5)
+    const penelopeX = this.penelope.x
+    const penelopeY = this.penelope.y
+
+    this.attackHitBox = this.add.rectangle(penelopeX, penelopeY, 30, 30, 0xffffff, 0.5)
     this.physics.add.existing(this.attackHitBox)
+    
 
     // collisions between acid drops and penelope
     this.physics.add.collider(this.seedGroup, this.attackHitBox, function (seedCollide) {
@@ -110,14 +133,11 @@ class GameSceneThree extends Phaser.Scene {
   }
   // slams semi truck on ferral avocado seed
   penelopeAttackRight() {
-    this.attackHitBox.x = this.penelope.x
-    this.attackHitBox.y = this.penelope.y
-    this.attackHitBox.velocity.x = 100
+    this.attackHitBox.body.velocity.x = 200
+    this.penelope.play('penelope_attack_right', true)
   }
   penelopeAttackLeft() {
-    this.attackHitBox.x = this.penelope.x 
-    this.attackHitBox.y = this.penelope.y
-    this.attackHitBox.velocity.x = 100
+    this.attackHitBox.body.velocity.x = -200
   }
 
   seedFollows () {
@@ -137,30 +157,65 @@ class GameSceneThree extends Phaser.Scene {
     {
       this.penelope.x = this.penelope.x - 5
       this.facingRight = false
+      this.penelope.play('penelope_walking_left', true)
     }
     else if (this.cursors.right.isDown)
     {
       this.penelope.x = this.penelope.x + 5
       this.facingRight = true
+      this.penelope.play('penelope_walking_right', true)
     }
     
 
-    if (this.cursors.up.isDown)
+    if (this.cursors.up.isDown && this.facingRight === true)
     {
       this.penelope.y = this.penelope.y - 5
+      this.penelope.play('penelope_walking_right', true)
     }
-    else if (this.cursors.down.isDown) {
+    else if (this.cursors.up.isDown && this.facingRight === false)
+    {
+        this.penelope.y = this.penelope.y - 5
+        this.penelope.play('penelope_walking_left', true)
+    }
+    else if (this.cursors.down.isDown && this.facingRight === false) {
       this.penelope.y = this.penelope.y + 5
+      this.penelope.play('penelope_walking_left', true)
+    }
+    else if (this.cursors.down.isDown && this.facingRight === true) {
+      this.penelope.y = this.penelope.y + 5
+      this.penelope.play('penelope_walking_right', true)
     }
     if (this.cursors.space.isDown && this.facingRight === true) {
       this.penelopeAttackRight()
     } 
     else if (this.cursors.space.isUp) {
-      this.attackHitBox.x = 0
-      this.attackHitBox.y = 0
+      this.attackHitBox.x = this.penelope.x
+      this.attackHitBox.y = this.penelope.y
+      this.attackHitBox.body.velocity.x = 0
+      this.penelope.body.setOffset(200, 650)
     }
     if (this.cursors.space.isDown && this.facingRight === false) {
       this.penelopeAttackLeft()
+      this.penelope.body.setOffset(670, 650)
+      this.penelope.play('penelope_attack_left', true)
+      this.penelope.on('animationcomplete', () => {
+        this.penelope.body.setOffset(200, 650)
+      })
+    }
+    if (this.cursors.space.isDown && this.cursors.left.isDown) {
+      this.penelope.play('penelope_attack_left', false)
+    }
+    else if (this.cursors.space.isDown && this.cursors.right.isDown) {
+      this.penelope.play('penelope_attack_right', false)
+      this.penelope.body.setOffset(200, 650)
+    }
+    if ((this.cursors.right.isUp && this.cursors.left.isUp) && (this.facingRight === false && this.cursors.space.isUp)) {
+      this.penelope.play('penelope_anim_standing_left', true)
+      this.penelope.body.setOffset(200, 650)
+    }
+    if ((this.cursors.right.isUp && this.cursors.left.isUp) && (this.facingRight === true && this.cursors.space.isUp)) {
+      this.penelope.play('penelope_anim_standing_right', true)
+      this.penelope.body.setOffset(200, 650)
     }
   } 
 }
